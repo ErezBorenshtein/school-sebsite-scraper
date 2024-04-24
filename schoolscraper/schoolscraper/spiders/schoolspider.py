ec90f11b4e2875641e5588b16334041e19bf1608
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-04-24 13:52:25 +0300

    Add writing files individually + arr of all dfs from all classes

--- schoolscraper/schoolscraper/spiders/schoolspider.py
+++ schoolscraper/schoolscraper/spiders/schoolspider.py
@@ -1,6 +1,5 @@
 import scrapy
 from scrapy import FormRequest
-from scrapy.utils.response import open_in_browser
 import pandas as pd
 import re
 
@@ -12,18 +11,21 @@ class SchoolSpider(scrapy.Spider):
     def __init__(self, classNumber=1, *args, **kwargs):
         super(SchoolSpider, self).__init__(*args, **kwargs)
         self.classNumber = classNumber
-        self.final_df = pd.DataFrame
+        self.final_dfs = []
 
     def parse(self, response):
         classes_values = response.css("select#dnn_ctr7126_TimeTableView_ClassesList option::attr(value)").extract()
+
+        self.log(f'\nclasses values: {classes_values}\n')
 
         for value in classes_values:
             data = {
                 '__EVENTTARGET': 'dnn$ctr7126$TimeTableView$btnTimeTable',
                 'dnn$ctr7126$TimeTableView$ClassesList': value,
-                'dnn$ctr7126$TimeTableView$ControlId': "8",
+                'dnn$ctr7126$TimeTableView$ControlId': "8"
             }
-            yield FormRequest.from_response(response, formdata=data, callback=self.parse_school)
+            self.log(f'Now logging {value}..')
+            yield FormRequest.from_response(response, formdata=data, callback=self.parse_school, meta={'value': value})
 
     def parse_school(self, response):
         def extract_text_in_parentheses(text):
@@ -33,7 +35,12 @@ class SchoolSpider(scrapy.Spider):
 
         dfs = pd.read_html(response.text)
         for i, df in enumerate(dfs):
-            if i == 11:
+            if i == 11:  # Assuming the 11th DataFrame contains schedule data
                 df.drop(columns=0, inplace=True)
                 df.drop(0, inplace=True)
                 df = df.applymap(lambda x: extract_text_in_parentheses(str(x)))
+                self.final_dfs.append(df)
+
+    def closed(self, reason):
+        for i, df in enumerate(self.final_dfs):
+            df.to_csv(f'schedule_{i}.csv', index=False)
