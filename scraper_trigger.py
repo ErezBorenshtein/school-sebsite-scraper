f8092e185765eb5fa9e1c29bad7ad89dd8f58bfa
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-04-26 00:31:13 +0300

    Add scraper_trigger.py

---
+++ scraper_trigger.py
@@ -0,0 +1,9 @@
+import os
+
+
+def start():
+    command1 = 'cd ./schoolscraper/schoolscraper/spiders/'
+    command2 = 'scrapy crawl schoolspider'
+
+    os.system(command1)
+    os.system(command2)
