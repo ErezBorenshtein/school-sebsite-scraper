12411ae503ccf7f2cdf1a393a20cf3a026fcb1de
rakrakon <68773850+rakrakon@users.noreply.github.com>
2024-04-25 22:13:37 +0300

    Removed unused import + cleanup

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
-import json
 import csv
-import discord
-from discord.ext import tasks
 import datetime
-from scrape import create_csv
+import json
+
+import discord
 import pytz
-import os
+from discord.ext import tasks
+
+from scrape import create_csv
 
 
 def get_empty_rooms(csv_file, json_file, day="Sunday", hour=1):
     hour = int(hour)
 
     # Parse JSON file to get all possible rooms
-    with open(json_file, 'r') as f:
-        possible_rooms = json.load(f)
+    with open(json_file, 'r') as file:
+        possible_rooms = json.load(file)
 
     # Create a dictionary mapping day strings to their respective column indices
     day_mapping = {
@@ -26,8 +27,8 @@ def get_empty_rooms(csv_file, json_file, day="Sunday", hour=1):
     }
 
     # Parse CSV file to get used rooms for the specified day and hour
-    with open(csv_file, 'r') as f:
-        reader = csv.reader(f)
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
         next(reader)  # Skip header
         for row in reader:
             if int(row[0]) == hour:
@@ -35,8 +36,8 @@ def get_empty_rooms(csv_file, json_file, day="Sunday", hour=1):
                 used_rooms = set(row[day_index].split(',')) if row[day_index] else set()
                 break
         else:
-          # If no data found for the specified day and hour, return all possible rooms
-          return possible_rooms
+            # If no data found for the specified day and hour, return all possible rooms
+            return possible_rooms
 
     # Find rooms that are not used
     empty_rooms = [room for room in possible_rooms if room not in used_rooms]
@@ -45,16 +46,15 @@ def get_empty_rooms(csv_file, json_file, day="Sunday", hour=1):
 
 def get_day():
     israel_tz = pytz.timezone('Israel')
-    
+
     current_date_israel = datetime.datetime.now(israel_tz)
-    #print("current_date_israel:", current_date_israel)
-    
-    day_of_week_int = current_date_israel.weekday() + 1  #! I dont know why the +1 is needed
-    #print("day_of_week_int:", day_of_week_int)
-    
+
+    day_of_week_int = current_date_israel.weekday() + 1  # ! I don't know why the +1 is needed
+
     day_of_week_str = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][day_of_week_int]
-    
+
     return day_of_week_str
+
 
 def get_hour():
     israel_tz = pytz.timezone('Israel')
@@ -74,7 +74,7 @@ def get_hour():
         9: (datetime.time(15, 15), datetime.time(16, 0)),
         10: (datetime.time(16, 0), datetime.time(16, 45)),
         11: (datetime.time(16, 45), datetime.time(17, 30)),
-  }
+    }
 
     # Find the closest hour in the school schedule
     closest_hour = None
@@ -84,7 +84,8 @@ def get_hour():
             if start_time <= current_time <= end_time:
                 return hour
             else:
-                difference = abs((start_time.hour * 60 + start_time.minute) -(current_time.hour * 60 + current_time.minute))
+                difference = abs(
+                    (start_time.hour * 60 + start_time.minute) - (current_time.hour * 60 + current_time.minute))
                 if difference < min_difference:
                     min_difference = difference
                     closest_hour = hour
@@ -98,6 +99,7 @@ def get_hour():
 
 client = discord.Client(intents=discord.Intents.all())
 
+
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
@@ -106,17 +108,15 @@ async def on_ready():
     print(2)
 
 
-
 @client.event
 async def on_message(message):
-    channel_id = 1229734808507256835 #My server channel id
+    channel_id = 1229734808507256835  # My server channel id
     if message.author == client.user:
         return
 
     if message.content.startswith("!help"):
-      
         embed = discord.Embed(
-            title= "Commands: " ,
+            title="Commands: ",
             description="!help\n2. !empty classes [Day] [hour]\n3. !empty classes today [hour]\n4. !empty classes",
             color=discord.Color.blue()
         )
@@ -124,14 +124,13 @@ async def on_message(message):
         channel = client.get_channel(channel_id)
         await channel.send(embed=embed)
 
-
     if message.content.startswith('!empty classes'):
         new_message = message.content.split(" ")
-        if len(new_message) > 2 and new_message[2].lower() != "today": #*empty classes [Day] [hour]
+        if len(new_message) > 2 and new_message[2].lower() != "today":  # *empty classes [Day] [hour]
             classes = get_empty_rooms("schedule.csv", "available_classes_ironih.json", new_message[2], new_message[3])
 
             embed = discord.Embed(
-                title= f"The empty rooms in {new_message[2]} at hour {new_message[3]} are:",
+                title=f"The empty rooms in {new_message[2]} at hour {new_message[3]} are:",
                 description=', '.join(classes),
                 color=discord.Color.blue())
 
@@ -139,10 +138,10 @@ async def on_message(message):
             await channel.send(embed=embed)
 
         else:
-            if len(new_message) == 4:  #*empty classes today [hour]
+            if len(new_message) == 4:  # *empty classes today [hour]
                 classes = get_empty_rooms("schedule.csv", "available_classes_ironih.json", get_day(), new_message[3])
                 embed = discord.Embed(
-                    title= f"The empty rooms today({get_day()}) at hour {new_message[3]} are:",
+                    title=f"The empty rooms today({get_day()}) at hour {new_message[3]} are:",
                     description=', '.join(classes),
                     color=discord.Color.blue())
 
@@ -153,7 +152,7 @@ async def on_message(message):
                 if closest_hour is not None and get_day() != "Saturday":
                     classes = get_empty_rooms("schedule.csv", "available_classes_ironih.json", get_day(), closest_hour)
                     embed = discord.Embed(
-                        title= f"The empty rooms today({get_day()}) at hour {closest_hour} are:",
+                        title=f"The empty rooms today({get_day()}) at hour {closest_hour} are:",
                         description=', '.join(classes),
                         color=discord.Color.blue()
                     )
@@ -161,6 +160,7 @@ async def on_message(message):
                     await channel.send(embed=embed)
                 else:
                     await message.channel.send("school has finished today")
+
 
 @tasks.loop(hours=24)
 async def scrape():
@@ -171,8 +171,9 @@ async def scrape():
         print(f"Error occurred during scraping: {e}")
         return
 
-#TOKEN = os.environ['TOKEN'] #! For my replit server
+
+# TOKEN = os.environ['TOKEN'] #! For my replit server
 with open("token.txt", "r") as f:
     TOKEN = f.read()
-#keep_alive() #! To keep the bot alive
+# keep_alive() #! To keep the bot alive
 client.run(TOKEN)
